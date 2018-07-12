6fff49ddc057d1a7e661312d4290e3a6eaea2b03
AlexeyShevchuk <alexolshevchuk@outlook.com>
2018-07-12 20:33:10 +0300

    add angular and core

---
+++ Vacations/Controllers/AuthController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Vacations.Controllers
+{
+    public class User
+    {
+        public string Name { get; }
+        public string Pass { get; }
+        public string Role { get; }
+
+        public User(string name, string pass, string role)
+        {
+            Name = name;
+            Pass = pass;
+            Role = role;
+        }
+    }
+
+    [Produces("application/json")]
+    [Route("api/Auth")]
+    public class AuthController : Controller
+    {
+        User[] _users = new User[]
+        {
+            new User("Alex", "pass", "admin"),
+            new User("Taras", "pass", "teamlead"),
+            new User("Luda", "pass", "user"),
+        };
+
+        [HttpPost("token")]
+        public IActionResult Token()
+        {
+            //string tokenString = "test";
+            var header = Request.Headers["Authorization"];
+            if (header.ToString().StartsWith("Basic"))
+            {
+                var credValue = header.ToString().Substring("Basic ".Length).Trim();
+                var usernameAndPassenc = Encoding.UTF8.GetString(Convert.FromBase64String(credValue)); //admin:pass
+                var usernameAndPass = usernameAndPassenc.Split(":");
+                //check in DB username and pass exist
+                foreach (var user in _users)
+                {
+                    if (usernameAndPass[0] == user.Name && usernameAndPass[1] == user.Pass)
+                    {
+                        var claimsdata = new[] { new Claim(ClaimTypes.Name, usernameAndPass[0]), new Claim(ClaimTypes.Role, user.Role), };
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ahbasshfbsahjfbshajbfhjasbfashjbfsajhfvashjfashfbsahfbsahfksdjf"));
+                        var signInCred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+                        var token = new JwtSecurityToken(
+
+                            issuer: "mysite.com",
+                            audience: "mysite.com",
+                            expires: DateTime.Now.AddMinutes(1),
+                            claims: claimsdata,
+                            signingCredentials: signInCred
+                        );
+                        var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                        return Ok(tokenString);
+                    }
+                }
+            }
+            return BadRequest("wrong request");
+
+            // return View();
+        }
+
+        [HttpGet("test1")]
+        public string GetTest1()
+        {
+            return "non authorized";
+        }
+
+        [Authorize]
+        [HttpGet("test2")]
+        public string GetTest2()
+        {
+            return "authorized";
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet("test3")]
+        public string GetTest3()
+        {
+            return "authorized with role admin";
+        }
+    }
+}
--- Vacations/Controllers/ValuesController.cs
+++ Vacations/Controllers/ValuesController.cs
@@ -1,37 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Vacations.Controllers
 {
-    public class ValuesController : ApiController
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        public class Book
         {
-            return new string[] { "value1", "value2" };
+            public string id;
+            public string title;
+            public string author;
+            public bool isFavorite;
+            public string imageUrl;
+            public int price;
+        }
+
+        public Book[] books = new Book[] {
+            new Book()
+            {
+                id = "1",
+                title = "The Universe in a Nutshell",
+                author = "Stephen Hawkin",
+                isFavorite = true,
+                imageUrl = "assets/images/1.jpg",
+                price = 50
+            },
+            new Book()
+            {
+                id = "2",
+                title = "JavaScript Patterns",
+                author = "Stoyan Stefanov",
+                isFavorite = true,
+                imageUrl = "assets/images/2.jpg",
+                price = 38
+            },
+
+            new Book()
+            {
+                id = "3",
+                title = "Angular: Up and Running: Learning Angular, Step by Step",
+                author = "Shyam Seshadri",
+                isFavorite = false,
+                imageUrl = "assets/images/3.jpg",
+                price = 47
+            },
+        };
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return books;
         }
 
         // GET api/values/5
-        public string Get(int id)
+        [HttpGet("{id}")]
+        public Book Get(int id)
         {
-            return "value";
+            return books[id];
         }
 
         // POST api/values
+        [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
         // PUT api/values/5
+        [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
         // DELETE api/values/5
+        [HttpDelete("{id}")]
         public void Delete(int id)
         {
         }
