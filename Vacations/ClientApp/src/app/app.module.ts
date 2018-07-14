a5da07b9cf08240cda13a41b4450a30e5cd30a9d
AlexeyShevchuk <alexolshevchuk@outlook.com>
2018-07-14 09:40:55 +0300

    Move to Core 2.1. and Add login page

--- Vacations/ClientApp/src/app/app.module.ts
+++ Vacations/ClientApp/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { NavMenuComponent } from './home/nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { CounterComponent } from './home/counter/counter.component';
+import { FetchDataComponent } from './home/fetch-data/fetch-data.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
@@ -16,16 +17,20 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
+      { path: 'auth', component: AuthComponent, pathMatch: 'full' },
       { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'auth', component: AuthComponent },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: '**', component: HomeComponent }
     ])
   ],
   providers: [],
