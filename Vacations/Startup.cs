44da4dd162feac04ecb49088e197ad7e151efc56
AlexeyShevchuk <alexeyolegovichsh@gmail.com>
2018-07-19 15:45:13 +0300

    Add VacationsDBConn sc

--- Vacations/Startup.cs
+++ Vacations/Startup.cs
@@ -25,7 +25,7 @@ namespace Vacations
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<VacationsDBContext>(options => options.UseSqlServer(Configuration.GetSection("VacationsDBConn").Value));
+            services.AddDbContext<VacationsDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("VacationsDBConn")));
 
             services.AddCors(options =>
             {
