7adb81bd25c2521fc3d0f82ff535cf160a74b5e0
AlexeyShevchuk <alexeyolegovichsh@gmail.com>
2018-07-19 12:58:21 +0300

    Update to Angular6

--- Vacations/ClientApp/src/app/profile/employee-profile/get-employee.service.ts
+++ Vacations/ClientApp/src/app/profile/employee-profile/get-employee.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 
 import { Employee } from './employee.model';
+import { Observable } from 'rxjs';
 
 const requestUrl = 'https://btangular.azurewebsites.net/api/employees/122538d2-493a-4dfd-bdbc-1ef05b022672';
 
