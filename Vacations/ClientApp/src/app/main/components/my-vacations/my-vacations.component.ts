35bbaedf39d729301b3abfe0a151731edc69d441
Tarasqrtv <fedorenkotv16@gmail.com>
2018-07-19 17:53:28 +0300

    I do not know what i can do this border my vacations

---
+++ Vacations/ClientApp/src/app/main/components/my-vacations/my-vacations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-vacations',
+  templateUrl: './my-vacations.component.html',
+  styleUrls: ['./my-vacations.component.scss']
+})
+export class MyVacationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
