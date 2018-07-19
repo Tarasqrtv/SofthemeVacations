2f2d5e5793fe3fe8573b1bf85902759db941bdee
LiudmylaHabit <vohony1@gmail.com>
2018-07-19 18:01:57 +0300

    Separating production and development HTTP URLs using environment

--- Vacations/ClientApp/src/app/main/services/employee.service.ts
+++ Vacations/ClientApp/src/app/main/services/employee.service.ts
@@ -3,14 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 import { Employee } from '../components/profile/employee.model';
-
-const requestUrl = 'https://btangular.azurewebsites.net/api/employees/122538d2-493a-4dfd-bdbc-1ef05b022672';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class EmployeeService {
     constructor (private http: HttpClient) { }
 
     getEmployee(): Observable<Employee> {
+        let requestUrl = environment.baseUrl + '/employees/122538d2-493a-4dfd-bdbc-1ef05b022672';
         return this.http.get<Employee>(`${requestUrl}`);
     }
 }
--- Vacations/ClientApp/src/app/main/services/vacation.service.ts
+++ Vacations/ClientApp/src/app/main/services/vacation.service.ts
@@ -2,15 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
+import {environment} from '../../../environments/environment';
 import { Vacation } from '../components/vacation/vacation.model';
-
-const requestUrl = 'http://btangular.azurewebsites.net/api/vacations/';
 
 @Injectable()
 export class VacationService {
     constructor (private http: HttpClient) { }
 
     getVacations(): Observable<Vacation[]> {
+        let requestUrl = environment.baseUrl + '/vacations/';
         return this.http.get<Vacation[]>(`${requestUrl}`);
     }
 }
