0a37b07619febb3c7efc518c15391120254975e8
LiudmylaHabit <vohony1@gmail.com>
2018-07-31 11:14:03 +0300

    authorizing guard works (without role)

--- Vacations.API/ClientApp/src/app/app.routes.ts
+++ Vacations.API/ClientApp/src/app/app.routes.ts
@@ -3,13 +3,16 @@ import { AuthComponent } from './auth/auth.component';
 import { MainComponent } from './main/main.component';
 
 import { AuthGuardService } from './auth/auth.guard';
+import { MainRoutes } from './main/main.routes';
+import { ProfileComponent } from './main/components/profile/profile.component';
 
 export const AppRoutes: Routes = [
     { path: 'auth', component: AuthComponent },
-    { path: '', redirectTo: 'main', pathMatch: 'full',
-        canActivate: [AuthGuardService]  },
-    { path: 'main', component: MainComponent,
-        canActivate: [AuthGuardService] },
-    { path: '**', component: MainComponent,
-        canActivate: [AuthGuardService]  }
+    {
+        path: '',
+        component: MainComponent,
+        canActivate: [AuthGuardService]
+    },
+    { path: '', redirectTo: '/profile', pathMatch: 'full' },
+    { path: '**', redirectTo: '/profile'} //component: MainComponent  }
 ];
